c3d40b4211d77fb772eff1505d90b4d2c5dde70d
domzxc2341 <80136346+domzxc2341@users.noreply.github.com>
2022-06-04 13:11:43 -0800

    ye
    ye

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -1,7 +1,32 @@
 import '../styles/globals.css'
+import style from '../styles/Home.module.css'
+import React, { useState } from 'react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+
+function MyApp(){
+
+function countini (){
+  return 4
+
+}
+ const [count, setdaw] = useState(countini())
+
+ function decrementCount () {
+   setdaw(prev => prev - 1)
+ }
+ function incrementCount () {
+  setdaw(prev => prev + 1)
+}
+return (
+
+  <div   className={style.container1}>
+  <button onClick={decrementCount} className={style.button1}>-</button>
+  <span className={style.button1}>{count}</span>
+  <button onClick={incrementCount} className={style.button1}>+</button>
+  </div>
+)
 }
 
 export default MyApp
